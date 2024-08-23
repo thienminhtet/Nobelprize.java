4b95a97049f5759bde66136155b27e6bd9c7df41
TheinGenius <theinminhtet44444@gmail.com>
2024-08-23 12:17:36 +0900

    Add files via upload

---
+++ August25.java
@@ -0,0 +1,14 @@
+public class August25{
+
+
+    public  static void main( String[]  ags ) { 
+	int[] arr = {2,45,2,553,222,5};
+	int max = arr[0];
+	for(int x : arr){
+	if(max < x){
+	max = x;
+}
+}
+	System .out. println( "Å‘å’l‚Í" + max  ) ;
+    }
+}
