054fc03839c0b776ff3e058251d20416ea7495a9
TheinGenius <theinminhtet44444@gmail.com>
2024-08-23 11:45:55 +0900

    Add files via upload

---
+++ Tmh.java
@@ -0,0 +1,9 @@
+public class Tmh{
+    public static void main(String[] args){
+        String x = "Python ";
+        String y = "is ";
+        String z = "awe some";
+     System.out.print(x + y + z);
+
+    }
+}
